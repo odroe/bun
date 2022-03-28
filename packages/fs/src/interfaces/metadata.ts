65abe2fa89e0aca0d3871483d952775a2a07e459
Seven Du <whine.whitish.0u@icloud.com>
2022-03-28 12:14:09 +0800

    :sparkles: Extra metadata for splitting directories and files

--- packages/fs/src/interfaces/metadata.ts
+++ packages/fs/src/interfaces/metadata.ts
@@ -1,20 +1,66 @@
 /**
- * Metadata extra properties.
+ * File metadata extra properties.
  */
-export interface MetadataExtra {
+export interface FileMetadataExtra {
+  /**
+   * Get file mime type.
+   */
   mimeType(): Promise<string>;
+
+  /**
+   * Get file `md5` hash.
+   */
   md5(): Promise<string>;
 }
 
+/**
+ * Directory metadata extra properties.
+ */
+export interface DirectoryMetadataExtra {}
+
+/**
+ * File or directory metadata extra.
+ */
+export type MetadataExtra<IS_FILE extends boolean> = {} & (IS_FILE extends true ? FileMetadataExtra : DirectoryMetadataExtra);
+
+/**
+ * File or directory metadata.
+ * 
+ * @see {@link FileMetadataExtra} and {@link DirectoryMetadataExtra}
+ */
 export interface Metadata {
+  /**
+   * Is file, return `true`.
+   */
   isFile(): Promise<boolean>;
+
+  /**
+   * Is directory, return `true`.
+   */
   isDirectory(): Promise<boolean>;
 
+  /**
+   * Get file or directory created time.
+   */
   createdAt(): Promise<Date>;
+
+  /**
+   * Get file or directory modified time.
+   */
   updatedAt(): Promise<Date>;
 
+  /**
+   * Get file or directory size.
+   */
   length(): Promise<number>;
 
-  get extra(): MetadataExtra | undefined;
+  /**
+   * Get file or directory extra properties.
+   */
+  get extra(): Promise<MetadataExtra<boolean>>;
+
+  /**
+   * Get file or directory in filesystem path.
+   */
   get path(): string;
 }
