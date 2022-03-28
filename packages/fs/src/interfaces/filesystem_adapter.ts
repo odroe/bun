7d836e18af6059fa881be27ff74ee62f51ac9365
Seven Du <whine.whitish.0u@icloud.com>
2022-03-28 11:30:24 +0800

    :memo: Add filesystem adapter comments

--- packages/fs/src/interfaces/filesystem_adapter.ts
+++ packages/fs/src/interfaces/filesystem_adapter.ts
@@ -1,4 +1,21 @@
 import { FilesystemReader } from './filesystem_reader';
 import { FilesystemWriter } from './filesystem_writer';
 
+/**
+ * Filesystem adapter interface.
+ * 
+ * Filesystem adapters are responsible for reading and writing files and
+ * directories.
+ * 
+ * ### Create a filesystem adapter
+ * ```ts
+ * import fs, { FilesystemAdapter } from '@odroe/fs';
+ * 
+ * export class NewAdapter implements FilesystemAdapter {
+ *   /// implementation
+ * }
+ * 
+ * fs.registerAdapter('new', new NewAdapter());
+ * ```
+ */
 export interface FilesystemAdapter extends FilesystemReader, FilesystemWriter {}
